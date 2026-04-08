0fc956fcc1b03ff5a0ba9330ceb7809bd08e8d97
JYPark0104 <01jun04@yonsei.ac.kr>
2026-04-08 11:52:58 +0000

    "version2 : Only sum만 출력하도록 변경"

--- test_code1.py
+++ test_code1.py
@@ -1,1 +1,8 @@
-a = 10; b = 5; print(f"Sum: {a+b}, Diff: {a-b}")
+# 기존 코드 Version 1
+# a = 10; b = 5; print(f"Sum: {a+b}, Diff: {a-b}")
+
+# 수정된 코드 Version 2
+a = 10
+b = 5
+
+print(f"Only Sum : {a+b}")
